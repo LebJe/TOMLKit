7ca574bdcf03161262a2704c84dfe4c34ec46195
furby™ <devs@wabi.foundation>
2023-12-10 11:41:39 -0600

    Fix unstable Checkit version for swift-5.5.

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -17,7 +17,7 @@ let package = Package(
 		// Use this dependency when one of the `TOMLTable` comparison tests fail and
 		// XCTAssertEqual tells you "<huge TOMLTable> is not equal to <other huge TOMLTable>"
 		// .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.0"),
-		.package(name: "Checkit", url: "https://github.com/karwa/swift-checkit.git", .branch("master")),
+		.package(name: "Checkit", url: "https://github.com/karwa/swift-checkit.git", from: "0.0.2"),
 	],
 	targets: [
 		.target(
